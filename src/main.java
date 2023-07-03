48a94d281d57e7e2b3159f71f0b6fbf209b56727
Taka <tk512003@gmail.com>
2023-07-03 15:04:11 -0400

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,13 @@
+class OddEvenChecker {
+    public static void main(String[] args) {
+
+        int number = 2;
+
+
+        if (number % 2 == 0) {
+            System.out.println(number + " is even.");
+        } else {
+            System.out.println(number + " is odd.");
+        }
+    }
+}
